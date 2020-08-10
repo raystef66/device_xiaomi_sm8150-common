291ca18c07ce013ae269bea317be6b19f846e38d
raystef66 <s.vanbarel@gmail.com>
2020-08-10 07:02:20 +0000

    cepheus: camera: change some packages

--- sm8150.mk
+++ sm8150.mk
@@ -101,7 +101,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
